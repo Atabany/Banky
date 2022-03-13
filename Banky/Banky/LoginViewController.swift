9eaf772ce42d3bf0cf6cc8c4ec75febbebf30edc
Atabany <mmemo4855@gmail.com>
2022-03-13 22:40:51 +0200

    setupNoStoryboard

---
+++ Banky/Banky/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Banky
+//
+//  Created by Mohamed Elatabany on 13/03/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let v = UIView(frame: UIScreen.main.bounds)
+
+    override func loadView() {
+        v.backgroundColor = .blue
+        view = v
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+
+}
+
