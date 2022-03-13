ea50daa907a07f428525e74aa6ad4dfd6f452cb2
Atabany <mmemo4855@gmail.com>
2022-03-13 22:28:29 +0200

    initial setup

---
+++ Banky/Banky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banky
+//
+//  Created by Mohamed Elatabany on 13/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
