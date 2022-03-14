16b5f4943d9dcef044ec08f5593096d5afac4df8
Atabany <mmemo4855@gmail.com>
2022-03-14 20:34:59 +0200

    refacotr: create generic onboarding view controller
    This commit adds a generic onboarding view controller thatn can be reused across multiple onboarding screens.

--- Banky/Banky/Onboarding/OnboardingViewController.swift
+++ Banky/Banky/Onboarding/OnboardingViewController.swift
@@ -10,17 +10,39 @@
 import UIKit
 class OnboardingViewController: UIViewController {
     
-    
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label     = UILabel()
     
+    
+    private var titleText: String
+    
+    
+    private var heroImageName: String
+    
+    
+    
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
+    
+    
+    
     
 }
 
@@ -36,7 +58,7 @@ extension OnboardingViewController {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode                               = .scaleAspectFit
-        imageView.image                                     = UIImage(named: "delorean")!
+        imageView.image                                     = UIImage(named: heroImageName)!
 
         
         // label
@@ -44,8 +66,8 @@ extension OnboardingViewController {
         label.textAlignment                                 = .center
         label.adjustsFontSizeToFitWidth                     = true
         label.numberOfLines                                 = 0
-        label.text                                          = "Banky is faster, easier to user, and has a brand new look and feel that will make you feel like you are back in 1989"
         label.font                                          = UIFont.preferredFont(forTextStyle: .title3)
+        label.text                                          = titleText
         
         
     }
---
+++ Banky/Banky/Utlis/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Banky
+//
+//  Created by Mohamed Elatabany on 14/03/2022.
+//
+
+import Foundation
+
+
+enum Constants {
+    
+    enum Images {
+        static let delorean = "delorean"
+        static let thumbs   = "thumbs"
+        static let world    = "world"
+    }
+    
+}
