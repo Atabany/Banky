9feae8b205b307f21cf81adaa4c76e673bec3f06
Atabany <mmemo4855@gmail.com>
2022-03-14 20:06:47 +0200

    add First onboarding screen

--- Banky/Banky/Onboarding/OnboardingViewController.swift
+++ Banky/Banky/Onboarding/OnboardingViewController.swift
@@ -5,113 +5,64 @@
 //  Created by Mohamed Elatabany on 14/03/2022.
 //
 
+
+
 import UIKit
-
-class OnboardingContainerViewController: UIViewController {
-
-    let pageViewController: UIPageViewController
-    var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-            
-        }
-    }
-
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
-
-        pages.append(page1)
-        pages.append(page2)
-        pages.append(page3)
-
-        currentVC = pages.first!
-
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
+class OnboardingViewController: UIViewController {
+    
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label     = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+}
 
-        view.backgroundColor = .systemPurple
 
-        addChild(pageViewController)
-        view.addSubview(pageViewController.view)
-        pageViewController.didMove(toParent: self)
+extension OnboardingViewController {
+    private func style() {
+        view.backgroundColor = .systemBackground
 
-        pageViewController.dataSource = self
-        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis                                      = .vertical
+        stackView.spacing                                   = 20
+        
+        // Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode                               = .scaleAspectFit
+        imageView.image                                     = UIImage(named: "delorean")!
+
+        
+        // label
+        label.translatesAutoresizingMaskIntoConstraints     = false
+        label.textAlignment                                 = .center
+        label.adjustsFontSizeToFitWidth                     = true
+        label.numberOfLines                                 = 0
+        label.text                                          = "Banky is faster, easier to user, and has a brand new look and feel that will make you feel like you are back in 1989"
+        label.font                                          = UIFont.preferredFont(forTextStyle: .title3)
+        
+        
+    }
+    
+    private func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+
+        view.addSubview(stackView)
 
         NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
-            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
-            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: 1)
         ])
-
-        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
-        currentVC = pages.first!
     }
 }
 
-// MARK: - UIPageViewControllerDataSource
-extension OnboardingContainerViewController: UIPageViewControllerDataSource {
-
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        return getPreviousViewController(from: viewController)
-    }
-
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return getNextViewController(from: viewController)
-    }
-
-    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
-        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
-        currentVC = pages[index - 1]
-        return pages[index - 1]
-    }
-
-    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
-        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
-        currentVC = pages[index + 1]
-        return pages[index + 1]
-    }
-
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return pages.count
-    }
-
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        return pages.firstIndex(of: currentVC) ?? 0
-    }
-}
-
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-    }
-}
 
