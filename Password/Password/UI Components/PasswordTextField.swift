c4d2c3f31eb82c2a794556cfdf7e7a7559f87660
Atabany <mmemo4855@gmail.com>
2022-03-21 20:09:12 +0200

    feat: add layout for password text field

---
+++ Password/Password/UI Components/PasswordTextField.swift
@@ -0,0 +1,167 @@
+//
+//  PasswordTextField.swift
+//  Password
+//
+//  Created by Mohamed Elatabany on 21/03/2022.
+//
+
+
+import UIKit
+class PasswordTextfield: UIView {
+    
+    let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill")!)
+    let textField = UITextField()
+    var placeHolderText: String
+    let eyeButton = UIButton(type: .custom)
+    let divderView = UIView()
+    let errorLabel = UILabel()
+    
+    
+    
+
+    init(placeHolderText: String) {
+        
+        self.placeHolderText = placeHolderText
+        
+        super.init(frame: .zero)
+        
+        style()
+        layout()
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 60)
+    }
+    
+    
+}
+
+
+extension PasswordTextfield {
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        lockImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.isSecureTextEntry = false // true
+        textField.placeholder = placeHolderText
+//        textField.delegate = self
+        textField.keyboardType = .asciiCapable
+        textField.attributedPlaceholder = NSAttributedString(string: placeHolderText,
+                                                             attributes: [.foregroundColor: UIColor.secondaryLabel])
+
+        eyeButton.translatesAutoresizingMaskIntoConstraints = false
+        eyeButton.setImage(UIImage(systemName: "eye.circle"), for: .normal)
+        eyeButton.setImage(UIImage(systemName: "eye.slash.circle"), for: .selected)
+        eyeButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
+        
+        
+        divderView.translatesAutoresizingMaskIntoConstraints = false
+        divderView.backgroundColor = UIColor.separator
+        
+        
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.textColor = .systemRed
+        errorLabel.font = .preferredFont(forTextStyle: .footnote)
+        errorLabel.text = "your password must meet the requirements below."
+        errorLabel.numberOfLines = 0
+        errorLabel.lineBreakMode = .byWordWrapping
+        errorLabel.isHidden = false
+
+        
+    }
+    
+    
+    func layout() {
+        addSubview(lockImageView)
+        addSubview(textField)
+        addSubview(eyeButton)
+        addSubview(divderView)
+        addSubview(errorLabel)
+
+        // Lock
+        NSLayoutConstraint.activate([
+            lockImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            lockImageView.centerYAnchor.constraint(equalTo: textField.centerYAnchor),
+        ])
+        
+        
+        // Textfield
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: topAnchor),
+            textField.leadingAnchor.constraint(equalToSystemSpacingAfter: lockImageView.trailingAnchor, multiplier: 1),
+
+        ])
+        
+        // eye Button
+        NSLayoutConstraint.activate([
+            eyeButton.centerYAnchor.constraint(equalTo: textField.centerYAnchor),
+            eyeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: textField.trailingAnchor, multiplier: 1),
+            eyeButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        
+        
+        
+        // Divider
+        NSLayoutConstraint.activate([
+            divderView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            divderView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            divderView.heightAnchor.constraint(equalToConstant: 1),
+            divderView.topAnchor.constraint(equalToSystemSpacingBelow: textField.bottomAnchor, multiplier: 1)
+        ])
+        
+        
+        
+        // Error label
+        NSLayoutConstraint.activate([
+            errorLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            errorLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            errorLabel.topAnchor.constraint(equalTo: divderView.bottomAnchor, constant: 4)
+        ])
+        
+
+        
+        
+        
+        
+        // CHCR
+        lockImageView.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
+        eyeButton.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
+        textField.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .horizontal)
+
+        
+        
+        
+                
+    }
+    
+}
+
+//MARK: - Actions
+
+
+extension PasswordTextfield {
+    @objc
+    func togglePasswordView() {
+        textField.isSecureTextEntry.toggle()
+        eyeButton.isSelected.toggle()
+
+    }
+    
+    
+}
+
+
+//MARK: - UITextfield delegate
+
+
--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let newPasswordTextField = PasswordTextfield(placeHolderText: "New Pass")
+    let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        style()
+        layout()
     }
-
-
+    
 }
+
+
+extension ViewController {
+    private func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        
+        newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+
+    }
+    
+    private func layout() {
+        
+        stackView.addArrangedSubview(newPasswordTextField)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ])
+        
+    }
+}
+
 
